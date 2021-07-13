89701e913b891a4fa2103250afcd44c2ff1f1e76
joseluis.munoz <joseluis.munoz@ibm.com>
2021-07-13 09:09:09 -0500

    add: scenario 2 test

--- src/module/game.test.js
+++ src/module/game.test.js
@@ -6,3 +6,10 @@ describe("Scenario 1: Game board creation", () => {
         .toBe("Game Board Creation... ||/n-+-+- || -+-+- ||/nBoard Created./nThe game will start with player X");
     });
 });
+
+describe("Scenario 2: Game board creation", () => {
+    it('Player X won with a vertical line ', () => {
+        expect(game.start())
+        .toBe("Player X:/nX| | /n-+-+-X|O| /n-+-+-X| |O/nPLAYER XWON!");
+    });
+});
